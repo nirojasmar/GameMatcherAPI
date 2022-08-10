2ee238c9e19105dd987384f2cf3787425b02ddb0
nirojasm <nirojasmar@gmail.com>
2022-08-10 16:50:00 -0500

    CRUD Interface

---
+++ GameMatcherAPI/Services/IUserDataService.cs
@@ -0,0 +1,16 @@
+﻿using GameMatcherAPI.Models;
+using MongoDB.Bson;
+
+namespace GameMatcherAPI.Services
+{
+    public interface IUserDataService
+    {
+        List<User> GetUsers();
+        User GetUserById(ObjectId id);
+        User InsertUser(User user);
+        User UpdateUser(User user);
+        bool DeleteUser(ObjectId id);
+        List<Rating> GetRatings();
+        UserGame GetUserGameInfo(ObjectId id);
+    }
+}
