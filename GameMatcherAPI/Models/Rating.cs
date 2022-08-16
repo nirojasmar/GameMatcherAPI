2f4c992c1e4fa672daadf5f7782e2aa6cb7cf64e
nirojasm <nirojasmar@gmail.com>
2022-08-16 16:16:55 -0500

    Rating BSON Data Updated to sync with DB

--- GameMatcherAPI/Models/Rating.cs
+++ GameMatcherAPI/Models/Rating.cs
@@ -6,12 +6,18 @@ namespace GameMatcherAPI.Models
 {
     public class Rating
     {
+        [BsonElement("rating_id")]
         public ObjectId Id { get; set; }
-        public double Stars { get; set; }
-        public string? Comment { get; set; }
 
-        [BsonElement("user")]
+        [BsonElement("rating_stars")]
+        public double Stars { get; set; }
+
+        [BsonElement("rating_text")]
+        public string? Comment { get; set; }
         public User? Author { get; set; }
+
+        [BsonElement("published_date")]
+        [BsonRepresentation(BsonType.DateTime)]
         public DateTime Date { get; set; }
 
         // Constructor for testing purposes TODO: Include Author into the constructor
